10840810c6e13ae82c2a2440ee192cef9ae25157
Nic11371 <nic11371@gmail.com>
2024-11-19 22:02:26 +0300

    search

--- example.py
+++ example.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from flask import render_template
 
 app = Flask(__name__)
+
+users = [
+    {'id': 1, 'name': 'mike'},
+    {'id': 2, 'name': 'mishel'},
+    {'id': 3, 'name': 'adel'},
+    {'id': 4, 'name': 'keks'},
+    {'id': 5, 'name': 'kamila'}
+]
 
 
 @app.route('/')
@@ -8,11 +17,34 @@ def hello_world():
     return 'Welcome to Nick!'
 
 
-@app.get('/users')
-def users_get():
-    return 'GET /users'
+@app.get('/users/')
+def get_users():
+    query = request.args.get('query', '')
+    filtered = [u for u in users if query in u['name']]
+    return render_template(
+        'users/index.html',
+        users=filtered,
+        search=query,
+    )
 
 
 @app.post('/users')
-def users_post():
-    return 'POST /users'
+def post_users():
+    return 'Users', 302
+
+
+@app.route('/courses/<id>')
+def courses_show(id):
+    return f'Course id: {id}'
+
+
+@app.route('/users/<id>')
+def show_user(id):
+    user = {
+        'id': id,
+        'name': f"user-{id}"
+    }
+    return render_template(
+        'users/show.html',
+        user=user,
+    )
