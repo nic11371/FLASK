7b2a7bec16dde6ae38e66223d72643e98bf67531
Nic11371 <nic11371@gmail.com>
2024-11-20 21:52:37 +0300

    create_user

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, redirect, request, url_for
 from flask import render_template
+import json
+import uuid
 
 app = Flask(__name__)
 
@@ -13,11 +15,11 @@ users = [
 
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Welcome to Nick!'
 
 
-@app.get('/users/')
+@app.get('/users')
 def get_users():
     query = request.args.get('query', '')
     filtered = [u for u in users if query in u['name']]
@@ -29,8 +31,40 @@ def get_users():
 
 
 @app.post('/users')
-def post_users():
-    return 'Users', 302
+def users_post():
+    user_data = request.form.to_dict()
+    errors = validate(user_data)
+    if errors:
+        return render_template(
+            'users/new.html',
+            user=user_data,
+            errors=errors
+        )
+    id = str(uuid.uuid4())
+    user = {
+        'id': id,
+        'name': user_data['name'],
+        'email': user_data['email']
+    }
+    users.append(user)
+    with open("./users.json", "w") as f:
+        json.dump(users, f)
+    return redirect(url_for('get_users'), code=302)
+
+
+@app.route('/users/new')
+def users_new():
+    user = {
+        'name': '',
+        'email': ''
+    }
+    errors = {}
+
+    return render_template(
+        'users/new.html',
+        user=user,
+        errors=errors
+    )
 
 
 @app.route('/courses/<id>')
@@ -48,3 +82,12 @@ def show_user(id):
         'users/show.html',
         user=user,
     )
+
+
+def validate(user):
+    errors = {}
+    if not user['name']:
+        errors['name'] = "Can't be blank"
+    if not user['email']:
+        errors['email'] = "Can't be blank"
+    return errors
